8280721489b890f759e6d07c17512a04bab2440d
kimjava911 <aibackend4th@gmail.com>
2025-12-15 12:12:43 +0900

    step07

--- src/main/java/kr/java/upload_exception/controller/S3FileController.java
+++ src/main/java/kr/java/upload_exception/controller/S3FileController.java
@@ -3,16 +3,20 @@ package kr.java.upload_exception.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,7 +36,28 @@ public class S3FileController {
                 .key(filename)
                 .build();
         try {
+            ResponseInputStream<GetObjectResponse> s3Object = s3Client
+                    .getObject(getObjectRequest);
+            GetObjectResponse s3ObjectResponse = s3Object.response(); // 객체 (메타데이터)
 
+            InputStreamResource resource = new InputStreamResource(s3Object); // 파일 그 자체
+
+            // 응답
+            HttpHeaders headers = new HttpHeaders();
+            // image/png, image/jpg ...
+            headers.setContentType(MediaType.parseMediaType(s3ObjectResponse.contentType()));
+            headers.setContentLength(s3ObjectResponse.contentLength());
+            // (주소로 접속 시) 이미지를 다운로드가 아니라 '바로 표시'
+//            headers.add(
+//                    HttpHeaders.CONTENT_DISPOSITION,
+//                    "inline; filename=\"" + filename + "\"");
+            headers.add(
+                    HttpHeaders.CONTENT_DISPOSITION,
+                    """
+                    inline; filename="%s"
+                    """.formatted(filename));
+
+            return new ResponseEntity<>(resource, headers, HttpStatus.OK);
         } catch (Exception e) {
             // 해당 파일이 없거나 다른 오류 발생
             log.error("파일 다운로드 실패 : {}", e.getMessage());
