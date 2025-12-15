235c59c3140ddfefae2531caae9bed406f71149d
kimjava911 <aibackend4th@gmail.com>
2025-12-15 11:31:12 +0900

    step06

--- src/main/java/kr/java/upload_exception/service/S3FileStorageService.java
+++ src/main/java/kr/java/upload_exception/service/S3FileStorageService.java
@@ -1,5 +1,6 @@
 package kr.java.upload_exception.service;
 
+import io.awspring.cloud.s3.S3Exception;
 import kr.java.upload_exception.exception.FileStorageException;
 import kr.java.upload_exception.exception.InvalidFileTypeException;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j // 로그 출력을 위한 연결
@@ -58,12 +65,48 @@ public class S3FileStorageService implements FileStorageService {
 
     @Override
     public String store(MultipartFile file) {
-        return "";
+        validateFile(file); // 파일을 검증
+        String extension = extractExtension(file.getOriginalFilename());
+        // 파일 확장자 추출
+        String key = UUID.randomUUID() + extension;
+
+        try {
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
+
+            s3Client.putObject(request,
+                    // software.amazon.awssdk.core.sync.RequestBody;
+                    RequestBody.fromInputStream(
+                            file.getInputStream(),
+                            file.getSize()
+                    ));
+
+            return key;
+        } catch (IOException | S3Exception e) {
+            throw new FileStorageException("S3 업로드 실패: " + e.getMessage());
+        }
     }
 
     @Override
     public void delete(String key) {
-
+        if (!StringUtils.hasText(key)) {
+            return;
+        }
+        try {
+            // S3 SDK
+            // Request -> S3Client
+            DeleteObjectRequest request = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(request);
+        } catch (S3Exception e) {
+            log.error("S3 삭제 실패: {}", e.getMessage());
+        }
     }
 
     @Override
